956de8aac0f5c9273c4dec342b284cb23983cbde
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-11-15 17:24:20 -0700

    setup bootstrap & forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HouseComponent } from './house/house.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   // list of components, directives, and pipes that belong to this module
-  declarations: [AppComponent, HouseComponent],
+  declarations: [AppComponent, HouseComponent, FormComponent],
   // list of modules that are imported into this module
   imports: [BrowserModule, AppRoutingModule],
   // list of services that are provided by this module
