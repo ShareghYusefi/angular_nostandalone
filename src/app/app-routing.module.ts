4ab9debc2ec7547dc8d6a39dedc7f9d1ae51642c
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-11-20 17:46:23 -0700

    routing to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // form
+  {
+    path: 'template-form',
+    component: FormComponent,
+  },
+  // reactive form
+  {
+    path: 'reactive-form',
+    component: ReactiveFormComponent,
+  },
+  // default route
+  {
+    path: '',
+    redirectTo: '/template-form',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
